e6e0c372898332e46255bdf93698d06e5b857089
ussamaismaili <ismailiossama@gmail.com>
2023-06-08 19:51:23 -0700

    edit 55

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    i = len(sys.argv) - 1
+	from sys import argv
 
-    if i == 0:
-        print("{} arguments.".format(i))
-    elif i == 1:
-        print("{} argument:".format(i))
-    else:
-        print("{} arguments:".format(i))
+	if len(argv) == 1:
+		print('{} arguments.'.format(len(argv)-1))
 
-    if i >= 1:
-        i = 0
-        for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
-            i += 1
+	elif len(argv) == 2:
+		print('{} argument:'.format(len(argv)-1))
+		print('1: {}'.format(argv[1]))
+
+	elif len(argv) > 2:
+		print('{} arguments:'.format(len(argv)-1))
+		for i in range(1, len(argv)):
+			print('{}: {}'.format(i, argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-    print(result)
+	from sys import argv
+
+	if len(argv) == 1:
+	print('{:d}'.format(len(argv)-1))
+
+	elif len(argv) > 1:
+	sum = 0
+	for i in range(1, len(argv)):
+		sum += int(argv[i])
+	print('{:d}'.format(sum))
