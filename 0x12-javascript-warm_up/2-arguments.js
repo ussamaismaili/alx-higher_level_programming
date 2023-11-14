1d8b1077631e45be0b1af253d7c132c66385d669
Oussama ismaili <ismailiossama@gmail.com>
2023-11-14 22:52:50 +0100

    quick edit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const count = process.argv.length;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
+const { argv } = require('node:process');
+(argv.length <= 2)? console.log("No argument"): console.log("Argument found");
