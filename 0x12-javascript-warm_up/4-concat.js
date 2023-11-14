6950a87ea9426566f1332856878f707798119621
Oussama ismaili <ismailiossama@gmail.com>
2023-11-14 23:57:05 +0100

    edit 7

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,4 +2,4 @@
 const { argv } = require('process');
 const argValue1 = argv.slice(2)[0];
 const argValue2 = argv.slice(3)[0];
-console.log(argValue1 , 'is', argValue2);
+console.log(argValue1 ,'is', argValue2);
